b7ef29c27036b980c87413dc84ae6c2c257a2c88
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2024-06-05 11:38:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_URL || 3000;
 
 const app = express();
 
